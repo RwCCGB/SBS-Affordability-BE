ea651c2a86aaf8abdf3605e8a1b40b80a3608c6d
waynekf <misterdotnet@hotmail.com>
2025-08-21 16:26:18 +0100

    Improved error handling for experimental ping API endpoint

--- src/controllers/pingController.ts
+++ src/controllers/pingController.ts
@@ -1,18 +1,28 @@
+import * as z from 'zod';
 import { Request, Response, NextFunction } from 'express';
 import Status from '../models/ping/status';
-import { PingRequest } from '../models/ping/pingRequest';
 import { PingResponse } from '../models/ping/pingResponse';
 import { pingRequestSchema } from '../schemas/schemas';
 
 export const ping = (req: Request, res: Response, next: NextFunction) => {
   try {
-    let myrequest: PingRequest = req.body;
+    const data = pingRequestSchema.safeParse(req.body);
 
-    const data = pingRequestSchema.parse(req.body);
+    const { success } = data;
 
-    /*TODO: implement correct functionality*/
-    let ping: PingResponse = { status: Status.Active, timestamp: new Date(), backendIsOnline: true, databaseIsonline: true };
-    res.json(ping);
+    if (success) {
+      let ping: PingResponse = { status: Status.Active, timestamp: new Date(), backendIsOnline: true, databaseIsonline: true };
+      res.json(ping);
+    } else {
+      let error : z.ZodError = data.error;
+      let prettyError = z.prettifyError(error);
+      const message = `Ping request schema validation failure. See below for extra detail: ${prettyError}.`;
+
+      return Promise.reject({
+        status: 400,
+        msg: message,
+      });
+    }
   } catch (error) {
     next(error);
   }
--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,5 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
+interface CustomError {
+  status?: number,
+  msg?: string,
+}
+
 export interface AppError extends Error {
   status?: number;
 }
@@ -13,4 +18,17 @@ export const errorHandler = (
   res.status(err.status || 500).json({
     message: err.message || 'Internal Server Error',
   });
+};
+
+export const schemaErrorHandler = (
+  err: AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (err.status === 400) {
+    /*TODO: tidy this up: I don't think I've done it the correct way.*/
+    const customError : CustomError = err;
+    res.status(err.status).json({message: customError.msg });
+  }
 };
