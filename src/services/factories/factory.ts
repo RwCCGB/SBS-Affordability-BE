b0741a3033a6d33623c512e9216cafebef4da7a9
RwCCGB <rwcc@hotmail.co.uk>
2025-08-27 14:47:23 +0100

    Added getIncomeTypes.json and getExpenditureTypes.json

--- src/services/factories/factory.ts
+++ src/services/factories/factory.ts
@@ -1,7 +1,8 @@
-import { LiveRegionService } from "../regions/liveRegionService";
-import {Region} from "../../models/regions/region"
 import config from "../../config/config";
 import {loadMock} from "../../utils/fileReader"
+
+import { LiveRegionService } from "../regions/liveRegionService";
+import {Region} from "../../models/regions/region"
 
 export function getRegionService() {
     if(config.useMocks){
