b4c314dc325c61ffdc3b1e13ea119343e5770fa4
RwCCGB <rwcc@hotmail.co.uk>
2025-08-27 15:17:39 +0100

    Added initial getExpenditureTypes tests

--- src/__test__/utils.test.ts
+++ src/__test__/utils.test.ts
@@ -1,7 +1,8 @@
 import { string } from "zod";
 import { loadMock } from "../utils/fileReader";
+import { stringFormat } from "zod/mini";
 
-describe("Testing utils functions", ()=>{
+describe("GetRegion Test Suit", ()=>{
     it("GetRegion: Read fileReader JSON mock data", async ()=>{
         const data = await loadMock<{name: string, id: Number}>("getRegion");
         expect(Array.isArray(data)).toBe(true);
@@ -20,6 +21,56 @@ describe("Testing utils functions", ()=>{
         expect(data).toEqual(
             expect.arrayContaining([
                 expect.objectContaining({id: 1, name: "Yorkshire & Humberside"})
+            ])
+        )
+    })
+})
+
+describe("GetIncomeTypes Test Suit", ()=>{
+    it("GetIncomeTypes: Read fileReader JSON mock data", async ()=>{
+        const data = await loadMock<{Text: string, id: Number}>("getIncomeTypes");
+        expect(Array.isArray(data)).toBe(true);
+        expect(data).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    id: expect.any(Number),
+                    Text: expect.any(String),
+                })
+            ])
+        )
+    })
+
+    it("GetIncomeTypes: Read fileReader JSON mock data and validate correct", async ()=>{
+        const data = await loadMock<IncomeType[]>("getIncomeTypes");
+        expect(data).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({id: 1, Text: "Guaranteed Other (Before Deductions)"})
+            ])
+        )
+    })
+})
+
+describe("GetExpenditureTypes Test Suit", ()=>{
+    it("GetExpenditureTypes: Read fileReader JSON mock data", async ()=>{
+        const data = await loadMock<{Text: string, SubText: string, Type: string, id: Number}>("getExpenditureTypes");
+        expect(Array.isArray(data)).toBe(true);
+        expect(data).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    id: expect.any(Number),
+                    Text: expect.any(String),
+                    SubText: expect.any(String),
+                    Type: expect.any(String)
+                })
+            ])
+        )
+    })
+
+    it("GetExpenditureTypes: Read fileReader JSON mock data and validate correct", async ()=>{
+        const data = await loadMock<ExpenditureType[]>("getExpenditureTypes");
+        expect(data).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({id: 2, Text: "Nursery/Child Care Costs", SubText: "Total child care costs including child care vouchers", Type: "M"})
             ])
         )
     })
